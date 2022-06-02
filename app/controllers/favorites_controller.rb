bced39774bf7ae19e4c3f8695c405a476e27936a
EC2 Default User <ec2-user@ip-172-31-41-162.ec2.internal>
2022-06-02 07:06:22 +0000

    [Finish]フォロー機能

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -2,13 +2,13 @@ class FavoritesController < ApplicationController
   def create
     @book_favorite = Favorite.new(user_id: current_user.id, book_id: params[:book_id])
     @book_favorite.save
-    redirect_to books_path
+    redirect_to request.referer
   end
 
   def destroy
     #find_byはid(主キー)か不明で、別の条件でレコードを検索したい場合
     @book_favorite = Favorite.find_by(user_id: current_user.id, book_id: params[:book_id])
     @book_favorite.destroy
-    redirect_to books_path
+    redirect_to request.referer
   end
 end
